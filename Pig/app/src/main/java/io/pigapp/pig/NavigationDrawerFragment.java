c836e15e76316e876d2da95edfe50f4c33d9b0c5
jgzuke <jgzuke@gmail.com>
2015-06-05 00:31:44 -0400

    Adding drawer options

--- Pig/app/src/main/java/io/pigapp/pig/NavigationDrawerFragment.java
+++ Pig/app/src/main/java/io/pigapp/pig/NavigationDrawerFragment.java
@@ -102,9 +102,10 @@ public class NavigationDrawerFragment extends Fragment {
                 android.R.layout.simple_list_item_activated_1,
                 android.R.id.text1,
                 new String[]{
-                        getString(R.string.title_section1),
-                        getString(R.string.title_section2),
-                        getString(R.string.title_section3),
+                        getString(R.string.main_activity_drawer_profile),
+                        getString(R.string.main_activity_drawer_fridge),
+                        getString(R.string.main_activity_drawer_mealplan),
+                        getString(R.string.main_activity_drawer_settings),
                 }));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
